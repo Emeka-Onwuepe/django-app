3e7fcad0f6c5085d41f02c785fce45060f10957a
casdon_mystery <pascalemy2010@gmail.com>
2020-02-22 13:26:52 +0100

    worked on static files settings

--- Magazine/settings.py
+++ Magazine/settings.py
@@ -17,7 +17,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -129,11 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Staticfiles'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # email settings
 EMAIL_BACKEND = emailinfo.EMAIL_BACKEND
@@ -155,6 +154,6 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-STATICFILES_STORAGE = "Magazine.aws_storage.StaticStorage"
+# STATICFILES_STORAGE = "Magazine.aws_storage.StaticStorage"
 
 django_heroku.settings(locals())
