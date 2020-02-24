91861295c4002c2ed587f3204b77f05d441c35c3
casdon_mystery <pascalemy2010@gmail.com>
2020-02-24 22:09:03 +0100

    editted the registration form

--- Magazine/settings.py
+++ Magazine/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["illumpedia.herokuapp.com"]
+ALLOWED_HOSTS = ["illumepedia.herokuapp.com"]
 
 
 # Application definition
