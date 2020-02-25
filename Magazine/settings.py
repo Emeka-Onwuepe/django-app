918310aff744db3aed6f9f11ece96eb0a29da0b3
casdon_mystery <pascalemy2010@gmail.com>
2020-02-25 12:59:28 +0100

    edited settings.py

--- Magazine/settings.py
+++ Magazine/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from . import emailinfo
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
